8a8bbeb95e9dfe81cbef5572b4e47677ee229756
Rely Arfadillah <shinmendenz@gmail.com>
2022-11-21 05:00:01 +0800

    test-button-branch

--- cabai.php
+++ cabai.php
@@ -13,7 +13,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Cabai</title>
+        <title>Cabai1</title>
         <link rel="stylesheet" href="assets/css/bawang.css?v=<?php echo time(); ?>">
     </head>
 
